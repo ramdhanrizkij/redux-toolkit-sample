18750fab30f4fd8d2aa5ed00a8a1d9f3355902c8
Pins ID <pinsid@pop-os.localdomain>
2023-04-29 17:33:57 +0700

    initial Commit

---
+++ src/components/ShowProduct.jsx
@@ -0,0 +1,47 @@
+import React, {useEffect} from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+import { getProducts, deleteProduct, productSelectors } from '../features/productSlice'
+import { Link } from 'react-router-dom'
+
+const ShowProduct = () => {
+  const dispatch = useDispatch();
+  const products = useSelector(productSelectors.selectAll)
+
+  useEffect(() => {
+    dispatch(getProducts())
+  
+  }, [dispatch])
+  
+  return (
+    <div className='box mt-5'>
+      <Link to="add" className='button is-success'>Add New</Link>        
+      <table className='table is-stripped is-fullwidth'>
+        <thead>
+          <tr>
+            <th>No</th>
+            <th>Title</th>
+            <th>Price</th>
+            <th>Actions</th>
+          </tr>
+        </thead>
+        <tbody>
+          {products.map((product, index)=> (
+            <tr key={product.id}>
+              <td>{index+1}</td>
+              <td>{product.title}</td>
+              <td>{product.price}</td>
+              <td>
+                <Link to={`edit/${product.id}`} className='button is-info is-small'>Edit</Link>
+                <button className='button is-danger is-small'
+                  onClick={()=> dispatch(deleteProduct(product.id))}>Delete</button>
+              </td>
+            </tr>
+          ))}
+          
+        </tbody>
+      </table>
+    </div>
+  )
+}
+
+export default ShowProduct
